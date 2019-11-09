4ab3edea09d21e1d9dfc3f27d4a95894fcfa0e56
Shivam Chaturvedi <shivam4095@gmail.com>
2019-11-09 19:41:45 +0530

    Added Dashboard

--- faculty/dashboard.php
+++ faculty/dashboard.php
@@ -3,7 +3,6 @@ session_start();
 require_once "./../sql_login/login.php";
 $conn = mysqli_connect($hostname,$username,$password,$database);
 $faculty_id = $_SESSION['username'];
-
 if (!$conn)
 {
     die('<p>Connection failed: <p>' . mysqli_connect_error());
@@ -26,7 +25,9 @@ if (!$conn)
                 <h1>Faculty Dashboard</h1>
             </div>
         </header>
-
+        <nav>
+            <a href="./../home/home.html"><input type="submit" value="Logout"></a>
+        </nav>
         <div class="sline"></div>
     <div class="sline"></div>
     <section class="outer-section">
@@ -43,21 +44,47 @@ if (!$conn)
                         {
                             $course_code = $row['course_code'];
                             echo "<li><input name=\"course\" type=\"radio\" value=$course_code>&emsp;".$row['course_name']."</li><br>";
+                        } 
+                    }
+                ?>
+                </ul>
+                    <ul>
+                        <li><input type="submit" class="submit-button" name="in" value="View/Attendance" /></li><br>
+                        <li><input type="submit" class="submit-button" name="in" value="Delete Course" onclick="return confirm('Are you sure?');" /></li>
+                    </ul>
+                </form>
+        </aside>
+        <section class="middle-pane">
+            <h1>Add Attendance</h1>
+            <h3>Select Course:-</h3>
+            <form action="./../faculty/attendance.php" method="post">
+                <ul>
+                <?php
+                    $query = "SELECT * FROM courses";
+                    $result = mysqli_query($conn, $query);
+                    while($row = mysqli_fetch_array($result))
+                    {
+                        if ($faculty_id == $row['faculty_id'])
+                        {
+                            $course_code = $row['course_code'];
+                            echo "<li><input name=\"course\" type=\"radio\" value=$course_code>&emsp;".$row['course_name']."</li><br>";
                         }
                     }
                 ?>
                 </ul>
                     <ul>
-                        <li><input type="submit" class="submit-button" name="in" value="View/Attendance" /></li>
+                        <li><input type="submit" class="submit-button" name="in" value="Add Attendance" /></li>
                     </ul>
-                </form>
-        </aside>
+            </form>
+		</section>
         <aside class="right-pane">
             <h1>Course Register</h1>
                 <form action="./../faculty/fcoursereg.php" method="post">
                     <ul>
                         <li>Course Name:- <input class="text-fields" type="text" name="cname" placeholder="Course Name" required/> </li>
                         <li>Course Code:- <input class="text-fields" type="text" name="ccode" placeholder="Course Code" required/></li>
+                        <li>Course Batch:- <input class="text-fields" type="text" name="cbatch" placeholder="Course Batch" required/></li>
+                        <li>Course Branch:- <input class="text-fields" type="text" name="cbranch" placeholder="Course Branch" required/></li>
                         <li><input type="submit" class="submit-button" name="in" value="Register" /></li>
                     </ul>
                 </form>
