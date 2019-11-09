94c36b4f09ccdf47929de2d429fef54385acb47e
Brinal Savsaviya <55130735+brinal8055@users.noreply.github.com>
2019-11-09 18:34:47 +0530

    Attendance details (#41)
    * Added automate file, sql_login and login
    
    * updated automate
    
    * updated stylesheet
    
    * Added backend for course float by faculty
    
    * Added option to check attendance for a course
    
    * Updated fcoursereg.php
    
    * Added Stylesheet
    
    * Completed Faculty
    
    * Added option to drop course and course attendance tables

---
+++ faculty/dashboard.php
@@ -0,0 +1,98 @@
+<?php
+session_start();
+require_once "./../sql_login/login.php";
+$conn = mysqli_connect($hostname,$username,$password,$database);
+$faculty_id = $_SESSION['username'];
+
+if (!$conn)
+{
+    die('<p>Connection failed: <p>' . mysqli_connect_error());
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Automated Attendance System</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0 shrink-to-fit=no">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/stylesheet.css" media="screen">
+    </head>
+
+    <body>
+        <header>
+            <div class="site_name">
+                <h1>Faculty Dashboard</h1>
+            </div>
+        </header>
+        <nav>
+            <a href="./../home/home.html"><input type="submit" value="Logout"></a>
+        </nav>
+        <div class="sline"></div>
+    <div class="sline"></div>
+    <section class="outer-section">
+        <aside class="left-pane">
+            <h1>Courses Floated</h1>
+                <form action="./../faculty/studentdetails.php" method="post">
+                <ul>
+                <?php
+                    $query = "SELECT * FROM courses";
+                    $result = mysqli_query($conn, $query);
+                    while($row = mysqli_fetch_array($result))
+                    {
+                        if ($faculty_id == $row['faculty_id'])
+                        {
+                            $course_code = $row['course_code'];
+                            echo "<li><input name=\"course\" type=\"radio\" value=$course_code>&emsp;".$row['course_name']."</li><br>";
+                        } 
+                    }
+                ?>
+                </ul>
+                    <ul>
+                        <li><input type="submit" class="submit-button" name="in" value="View/Attendance" /></li><br>
+                        <li><input type="submit" class="submit-button" name="in" value="Delete Course" onclick="return confirm('Are you sure?');" /></li>
+                    </ul>
+                </form>
+        </aside>
+        <section class="middle-pane">
+            <h1>Add Attendance</h1>
+            <h3>Select Course:-</h3>
+            <form action="./../faculty/attendance.php" method="post">
+                <ul>
+                <?php
+                    $query = "SELECT * FROM courses";
+                    $result = mysqli_query($conn, $query);
+                    while($row = mysqli_fetch_array($result))
+                    {
+                        if ($faculty_id == $row['faculty_id'])
+                        {
+                            $course_code = $row['course_code'];
+                            echo "<li><input name=\"course\" type=\"radio\" value=$course_code>&emsp;".$row['course_name']."</li><br>";
+                        }
+                    }
+                ?>
+                </ul>
+                    <ul>
+                        <li><input type="submit" class="submit-button" name="in" value="Add Attendance" /></li>
+                    </ul>
+            </form>
+		</section>
+        <aside class="right-pane">
+            <h1>Course Register</h1>
+                <form action="./../faculty/fcoursereg.php" method="post">
+                    <ul>
+                        <li>Course Name:- <input class="text-fields" type="text" name="cname" placeholder="Course Name" required/> </li>
+                        <li>Course Code:- <input class="text-fields" type="text" name="ccode" placeholder="Course Code" required/></li>
+                        <li>Course Batch:- <input class="text-fields" type="text" name="cbatch" placeholder="Course Batch" required/></li>
+                        <li>Course Branch:- <input class="text-fields" type="text" name="cbranch" placeholder="Course Branch" required/></li>
+                        <li><input type="submit" class="submit-button" name="in" value="Register" /></li>
+                    </ul>
+                </form>
+        </aside>
+    </section>
+    <footer>
+        <p>Faculty: <?php echo $_SESSION['username']; ?></p>
+    </footer>
+</body>
+</html>
---
+++ faculty/studentdetails.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+require_once "./../sql_login/login.php";
+
+$conn = mysqli_connect($hostname,$username,$password,$database);
+if(!$conn)
+{
+    echo "Connect Error:".mysqli_error($conn);
+}
+$faculty_id = $_SESSION['username'];
+$course_code = $_POST['course'];
+if($_POST['in']=="View/Attendance")
+{
+    echo "<h1>".$course_code." Attendance</h1>";
+    $query = "SELECT * FROM $course_code"."_attendance";
+    $result = mysqli_query($conn,$query);
+    echo "<table border=1>";
+    echo "<tr><th>Date</th><th>Present</th><th>Absent</th></tr>";
+    while($row = mysqli_fetch_array($result))
+    {
+        echo "<tr>";
+        echo "<td>";
+        echo $row['attendance_date'];
+        echo "</td>";
+        echo "<td>";
+        echo $row['present'];
+        echo "</td>";
+        echo "<td>";
+        echo $row['absentees'];
+        echo "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+else
+{
+    // echo $course_code;
+    $query = "DELETE FROM courses WHERE course_code = '$course_code'";
+    $result = mysqli_query($conn,$query);
+    if(!$result)
+    {
+        echo "Couldn't delete<br>".mysqli_error($conn);
+    }
+    else
+    {
+        echo "Course Deleted";
+        $query = "DROP TABLE $course_code";
+        $result = mysqli_query($conn,$query);
+        if(!$result)
+        {
+            echo "Course table couldn't be dropped. <br>";
+        }
+        $table_name = $course_code."_attendance";
+        $query = "DROP TABLE $table_name";
+        $result = mysqli_query($conn,$query);
+        if(!$result)
+        {
+            echo "Course attendance couldn't be dropped. <br>";
+        }
+    }
+}
+?>
