4e9a38315553f6833c2ce7b11b0b9e8f3bb73ff7
Shivam Chaturvedi <shivam4095@gmail.com>
2019-11-10 21:01:27 +0530

    Updated Automate

--- automate/automate.php
+++ automate/automate.php
@@ -14,10 +14,10 @@
     }
     finally{
         $db_create = "CREATE DATABASE {$database}";
-        if(mysqli_query($conn,$db_create))
-            echo "Database created.</br>";
-        else 
-            die("Error creating database.".mysqli_error($conn));
+        if(!mysqli_query($conn,$db_create))
+        echo "      <script>
+        alert('There is some error!');
+        </script>";
 
         mysqli_close($conn);
 
@@ -26,46 +26,48 @@
 
         $tables = "CREATE TABLE student_info (username varchar(10) NOT NULL PRIMARY KEY, password varchar (15) NOT NULL)";
 
-        if(mysqli_query($conn,$tables))
-            echo "Successfuly created student_info table. </br>";
-        else 
-            die("Error while making student_info table.".mysqli_error($conn));
-        
+        if(!mysqli_query($conn,$tables))
+            echo "      <script>
+            alert('There is some error!');
+            </script>";
+
         $tablea = "CREATE TABLE admin_info (username varchar(10) NOT NULL PRIMARY KEY, password varchar (15) NOT NULL)";
 
-        if(mysqli_query($conn,$tablea))
-            echo "Successfuly created admin_info table. </br>";
-        else 
-            die("Error while making admin_info table.".mysqli_error($conn));
-        
+        if(!mysqli_query($conn,$tablea))
+            echo "      <script>
+            alert('There is some error!');
+            </script>";
         $tablef = "CREATE TABLE faculty_info (username varchar(10) NOT NULL PRIMARY KEY, password varchar (15) NOT NULL, name varchar(256) NOT NULL)";
 
-        if(mysqli_query($conn,$tablef))
-            echo "Successfuly created faculty info table. </br>";
-        else 
-            die("Error while making faculty info table.".mysqli_error($conn));
-
+        if(!mysqli_query($conn,$tablef))
+            echo "      <script>
+            alert('There is some error!');
+            </script>";
+        
         $tableac = "CREATE TABLE courses (course_code varchar(10) NOT NULL PRIMARY KEY, course_name varchar(30) NOT NULL, faculty_id varchar (20) NOT NULL, no_of_students int(6) DEFAULT 0, batch year(4) NOT NULL, branch varchar(10) NOT NULL)";
 
-        if(mysqli_query($conn,$tableac))
-            echo "Successfuly created courses table. </br>";
-        else 
-            die("Error while making courses table.".mysqli_error($conn));
+        if(!mysqli_query($conn,$tableac))
+            echo "      <script>
+            alert('There is some error!');
+            </script>";
 
         $tableac = "CREATE TABLE students (username varchar(10) NOT NULL PRIMARY KEY, name varchar(20) NOT NULL, dob date NOT NULL, no_of_courses int(6) DEFAULT 0, branch varchar(10) NOT NULL, batch year(4) NOT NULL)";
 
-        if(mysqli_query($conn,$tableac))
-            echo "Successfuly created courses table. </br>";
-        else 
-            die("Error while making courses table.".mysqli_error($conn));
-
+        if(!mysqli_query($conn,$tableac))
+            echo "      <script>
+            alert('There is some error!');
+            </script>";
+       
         $admindata = "INSERT INTO admin_info VALUES(\"\admin\",\"admin\")";
-        if(mysqli_query($conn,$admindata))
-            echo "Successfuly added admin data. </br>";
-        else 
-            die("Error while adding admin data.".mysqli_error($conn));
+        if(!mysqli_query($conn,$admindata))
+            echo "      <script>
+            alert('There is some error!');
+            </script>";
         
-
+        echo "      <script>
+        alert('Successfully Created the Automated Files. Login on admin using details admin:admin');
+        window.location.href='./../hoome/home.html';
+        </script>";
     }
 
 ?>
